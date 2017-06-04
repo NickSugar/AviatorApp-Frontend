fe0a2af2c3cff86f86f5df401d5f09f3d8092eb8
NickSugar <nicksugar1@outlook.com>
2017-06-03 21:48:36 -0600

    toggled auth redirect url

--- public/app.config.js
+++ public/app.config.js
@@ -35,8 +35,8 @@
         domain: 'nicksugar.auth0.com',
         responseType: 'token id_token',
         audience: 'https://nicksugar.auth0.com/userinfo',
-        redirectUri: 'http://localhost:5000/premium',
-        // redirectUri: 'https://aviatorsapp.com/?utm_source=web_app_manifest#/premium',
+        // redirectUri: 'http://localhost:5000/premium',
+        redirectUri: 'https://aviatorsapp.com/?utm_source=web_app_manifest#/premium',
         scope: 'openid'
       });
 
