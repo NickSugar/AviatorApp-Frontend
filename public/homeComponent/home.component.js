854f291d3d9c4ee52648f2e107bd81b5dce8f97a
NickSugar <nicksugar1@outlook.com>
2017-06-05 12:20:46 -0600

    auth

--- public/homeComponent/home.component.js
+++ public/homeComponent/home.component.js
@@ -1,26 +1,15 @@
 (function() {
   'use strict'
 
-  var template = './homeComponent/home.html'
-
   angular.module('aviatorsApp')
     .component('homeComp', {
-      templateUrl: template,
+      templateUrl: './homeComponent/home.html',
       controller: ['authService', homeCtrl]
     });
 
   function  homeCtrl(authService) {
         const vm = this
 
-        vm.authenticated = authService.isAuthenticated()
-        console.log(vm.authenticated);
-
-        if (vm.authenticated) {
-          template = './homeComponent/premiumHome.html'
-        }else {
-          template = './homeComponent/home.html'
-        }
-
-        authService.authenticated = vm.authenticated
+        authService.authenticated = authService.isAuthenticated()
     }
 }())
