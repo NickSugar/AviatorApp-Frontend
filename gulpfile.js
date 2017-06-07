96dc2da4d8464b20c371fd9f5c29ef37d7968dae
NickSugar <nicksugar1@outlook.com>
2017-06-07 12:08:15 -0600

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ gulp.task('default', function(callback) {
 
   swPrecache.write(`${rootDir}/service-worker.js`, {
     staticFileGlobs: [
-      rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'
+      rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}',
+      rootDir + '/stylesheets/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'
     ],
     runtimeCaching: [{
       urlPattern: /min\.js/,
