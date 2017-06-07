5eb4bf4a00c0015adc80728bc3f2f80b642feae7
NickSugar <nicksugar1@outlook.com>
2017-06-06 18:26:23 -0600

    working on menu

--- public/premiumHomeComponent/premiumHome.component.js
+++ public/premiumHomeComponent/premiumHome.component.js
@@ -11,5 +11,32 @@
         const vm = this
 
         vm.auth = authService
+        vm.toggleMenu = toggleMenu
+        vm.goToPage = goToPage
+
+        var pages = new Array('one', 'two', 'three', 'four');
+        var menuClosed = true
+
+        function toggleMenu() {
+          if (menuClosed) {
+            document.getElementsByClassName('wrapper')[0].classList.toggle('menu-open');
+          }
+        }
+        toggleMenu()
+
+        function goToPage(page) {
+          var wrapper = document.getElementsByClassName('wrapper')[0];
+          var sections = document.getElementsByTagName('section');
+          for (var i = 0; i < sections.length; i++) {
+            sections[i].classList.remove('before','after');
+            if (i > page) {
+              sections[i].classList.add('after');
+            }
+          }
+          wrapper.classList.remove('menu-open','page-one','page-two');
+          wrapper.classList.add('page-' + pages[page]);
+          menuClosed = !menuClosed
+          toggleMenu()
+        }
     }
 }())
